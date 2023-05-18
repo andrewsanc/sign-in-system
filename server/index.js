60c67d4764a80eea884eda32a0c70c1e7310411b
Andrew Sanchez <druu831@gmail.com>
2023-05-18 16:00:14 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, console.log(`Server is running on port ${port}`));
