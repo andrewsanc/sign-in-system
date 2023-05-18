af093e741fb5712acede56ebde426efc56a7ad08
Andrew Sanchez <druu831@gmail.com>
2023-05-18 16:58:05 -0700

    added colors/cors. added mongodb setup/connection

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const colors = require("colors");
+const connectDB = require("./config/db");
 const dotenv = require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+connectDB();
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.post("/login", (req, res) => {
+  res.send("login");
 });
 
 app.listen(port, console.log(`Server is running on port ${port}`));
