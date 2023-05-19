8b46e023b4d27e5346dc34dfa3deecc777191ca3
Andrew Sanchez <druu831@gmail.com>
2023-05-18 17:14:07 -0700

    register/login route implementation

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const colors = require("colors");
 const connectDB = require("./config/db");
+const User = require("./models/user");
 const dotenv = require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -14,8 +16,40 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-app.post("/login", (req, res) => {
-  res.send("login");
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.redirect("/dashboard");
+    } else {
+      res.redirect("/");
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const { email, firstName, lastName, password } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+    });
+
+    await user.save();
+
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+  }
 });
 
 app.listen(port, console.log(`Server is running on port ${port}`));
